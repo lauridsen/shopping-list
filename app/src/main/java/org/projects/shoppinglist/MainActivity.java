7620eef1b23bb9e236f241435d25d070747d5bff
Michael Lauridsen <Pjuker@gmail.com>
2018-03-15 09:44:27 +0100

    init

---
+++ app/src/main/java/org/projects/shoppinglist/MainActivity.java
@@ -0,0 +1,86 @@
+package org.projects.shoppinglist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ArrayAdapter<String> adapter;
+    ListView listView;
+    ArrayList<String> bag = new ArrayList<>();
+
+    public ArrayAdapter getMyAdapter()
+    {
+        return adapter;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Needed to get the toolbar to work on older versions
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //getting our listiew - you can check the ID in the xml to see that it
+        //is indeed specified as "list"
+        listView = findViewById(R.id.list);
+        //here we create a new adapter linking the bag and the
+        //listview
+        adapter =  new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_checked,bag );
+
+        //setting the adapter on the listview
+        listView.setAdapter(adapter);
+        //here we set the choice mode - meaning in this case we can
+        //only select one item at a time.
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        Button addButton =  findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.add("Milk");
+                Log.d("Bag","Items in back: "+bag.size());
+            }
+        });
+
+        //add some stuff to the list so we have something
+        // to show on app startup
+        bag.add("Bananas");
+        bag.add("Apples");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
