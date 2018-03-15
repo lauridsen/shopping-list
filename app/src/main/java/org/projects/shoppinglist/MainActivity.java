687c9018dff3ff9f04a929bab6440ea22a18877f
Michael Lauridsen <Pjuker@gmail.com>
2018-03-15 12:37:05 +0100

    Added list functionality

--- app/src/main/java/org/projects/shoppinglist/MainActivity.java
+++ app/src/main/java/org/projects/shoppinglist/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -33,33 +34,45 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Did we have stuff in our bag?
+        if(savedInstanceState != null) {
+            ArrayList<String> savedBag = savedInstanceState.getStringArrayList("savedBag");
+            if(savedBag != null) {
+                bag = savedBag;
+            }
+        }
+
         //getting our listiew - you can check the ID in the xml to see that it
         //is indeed specified as "list"
         listView = findViewById(R.id.list);
         //here we create a new adapter linking the bag and the
         //listview
         adapter =  new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_checked,bag );
-
+                android.R.layout.simple_list_item_single_choice,bag );
         //setting the adapter on the listview
         listView.setAdapter(adapter);
         //here we set the choice mode - meaning in this case we can
         //only select one item at a time.
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
+        //Get input item
+        final EditText inputText = findViewById(R.id.inputItem);
+        final EditText inputQty = findViewById(R.id.inputSize);
+
         Button addButton =  findViewById(R.id.addButton);
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                adapter.add("Milk");
+                adapter.add(inputText.getText().toString() + " x " + inputQty.getText().toString());
                 Log.d("Bag","Items in back: "+bag.size());
             }
         });
 
         //add some stuff to the list so we have something
         // to show on app startup
-        bag.add("Bananas");
-        bag.add("Apples");
+//        bag.add("Bananas");
+//        bag.add("Apples");
 
     }
 
@@ -81,6 +94,31 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        else if (id == R.id.action_delete) {
+            int position = listView.getCheckedItemPosition();
+            //Check if items are available to be deleted
+            if (bag.size() != 0 && position != -1){
+                bag.remove(position);
+                adapter.notifyDataSetChanged();
+                listView.setItemChecked(-1, true);
+            }
+        }
+
+        else if (id == R.id.action_clear) {
+            if (bag.size() != 0) {
+                bag.clear();
+                adapter.notifyDataSetChanged();
+            }
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    //This method is called before our activity is destroyed
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+		/* Here we put code now to save the state */
+        outState.putStringArrayList("savedBag", bag);
     }
 }
